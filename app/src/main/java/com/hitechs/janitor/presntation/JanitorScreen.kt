df5cc0e24fcf212b149ece0c96c94ae995f3e49f
Keren pa <keren.pa@comuniverse.com>
2025-12-28 09:59:05 +0200

    make user input save in config change

--- app/src/main/java/com/hitechs/janitor/presntation/JanitorScreen.kt
+++ app/src/main/java/com/hitechs/janitor/presntation/JanitorScreen.kt
@@ -3,7 +3,6 @@ package com.hitechs.janitor.presntation
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,7 +14,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -24,16 +22,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.hitechs.janitor.R
 import com.hitechs.janitor.domain.GarbageBag
 import com.hitechs.janitor.domain.TripsResult
 
@@ -52,7 +49,7 @@ fun LoadedView(
     state: JanitorState.BagsLoaded,
     viewModel: JanitorViewModel
 ) {
-    var bagWeight by remember { mutableStateOf("") }
+    var bagWeight by rememberSaveable { mutableStateOf("") }
 
     LazyColumn(
         modifier = Modifier
